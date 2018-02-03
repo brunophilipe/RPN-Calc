1ae79d4fcc007c1778ecbb6050ccc81e59bfa6bf
Bruno Philipe Resende Silva <git@bruno.ph>
2018-02-03 14:56:31 +0100

    Initial Commit

---
+++ calc WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  calc WatchKit Extension
+//
+//  Created by Bruno Philipe on 3/2/18.
+//  Copyright © 2018 Bruno Philipe. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ calc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  calc
+//
+//  Created by Bruno Philipe on 3/2/18.
+//  Copyright © 2018 Bruno Philipe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
